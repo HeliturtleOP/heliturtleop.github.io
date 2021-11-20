ddd90395e5b8b5395f0294c61da3bfc78cc51f28
HeliturtleOP <oproulx8896@gmail.com>
2021-11-19 18:02:17 -0600

    IT FUCKING WORKED

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
-import * as THREE from './three.js-master/build/three.module.js'
+import * as THREE from './three.js-master/build/three.module.js';
+import { GLTFLoader } from './three.js-master/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from './three.js-master/examples/jsm/controls/OrbitControls.js';
 
-const canvas = document.querySelector('.webgl')
-const scene = new THREE.Scene()
+const loader = new GLTFLoader();
 
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.Material({
-    color: 0x00ff00
-})
-const boxMesh = new THREE.Mesh(geometry,material)
-scene.add(boxMesh)
 
-//boilerplate code
 
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+const scene = new THREE.Scene();
+			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 5000)
-camera.position.set(0,1,2)
-scene.add(camera)
+			const renderer = new THREE.WebGLRenderer();
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
 
-const renderer = new THREE.WebGL1Renderer({
-    canvas:canvas
-})
+            loader.load('./models/record.gltf', function (gltf){
+                scene.add(gltf.scene)
+            })
 
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
-renderer.shadowMap.enabled = true
-renderer.render(scene,camera)
+            const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+            scene.add( light );
+
+			const geometry = new THREE.BoxGeometry();
+			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			const cube = new THREE.Mesh( geometry, material );
+			//scene.add( cube );
+
+			camera.position.z = 500;
+
+            const controls = new OrbitControls( camera, renderer.domElement );
+
+            //controls.update() must be called after any manual changes to the camera's transform
+            camera.position.set( 0, 20, 100 );
+            controls.update();
+
+            renderer.outputEncoding = THREE.sRGBEncoding;
+
+			const animate = function () {
+				requestAnimationFrame( animate );
+
+
+
+				cube.rotation.x += 0.01;
+				cube.rotation.y += 0.01;
+
+                renderer.outputEncoding = THREE.sRGBEncoding;
+				renderer.render( scene, camera );
+			};
+
+			animate();
